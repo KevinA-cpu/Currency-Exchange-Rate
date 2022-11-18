73fa1a4f3672b949f350fef6cd8e00262a787b9d
Heisenburg <69845869+KevinA-cpu@users.noreply.github.com>
2022-11-18 09:08:13 +0700

    Create database.sql

---
+++ db/database.sql
@@ -0,0 +1,135 @@
+USE [master]
+GO
+/****** Object:  Database [socketserver]    Script Date: 11/18/2022 9:03:54 AM ******/
+CREATE DATABASE [socketserver]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'socketserver', FILENAME = N'D:\SQLserver\MSSQL15.MSSQLSERVER\MSSQL\DATA\socketserver.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'socketserver_log', FILENAME = N'D:\SQLserver\MSSQL15.MSSQLSERVER\MSSQL\DATA\socketserver_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [socketserver] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [socketserver].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [socketserver] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [socketserver] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [socketserver] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [socketserver] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [socketserver] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [socketserver] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [socketserver] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [socketserver] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [socketserver] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [socketserver] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [socketserver] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [socketserver] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [socketserver] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [socketserver] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [socketserver] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [socketserver] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [socketserver] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [socketserver] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [socketserver] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [socketserver] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [socketserver] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [socketserver] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [socketserver] SET RECOVERY FULL 
+GO
+ALTER DATABASE [socketserver] SET  MULTI_USER 
+GO
+ALTER DATABASE [socketserver] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [socketserver] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [socketserver] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [socketserver] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [socketserver] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [socketserver] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'socketserver', N'ON'
+GO
+ALTER DATABASE [socketserver] SET QUERY_STORE = OFF
+GO
+USE [socketserver]
+GO
+/****** Object:  Table [dbo].[currencyvcb]    Script Date: 11/18/2022 9:03:54 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[currencyvcb](
+	[date] [date] NULL,
+	[currency] [varchar](3) NULL,
+	[buy_cash] [float] NULL,
+	[buy_transfer] [float] NULL,
+	[sell] [float] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[loginserver]    Script Date: 11/18/2022 9:03:54 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[loginserver](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](50) NULL,
+	[password] [varchar](50) NULL,
+ CONSTRAINT [PK_loginserver] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[routine]    Script Date: 11/18/2022 9:03:54 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[routine](
+	[Id] [int] NOT NULL,
+	[date] [varchar](max) NULL,
+	[earnings] [int] NULL,
+	[exercise] [varchar](max) NULL,
+	[study] [varchar](max) NULL,
+	[diet] [varchar](max) NULL,
+	[python] [varchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [socketserver] SET  READ_WRITE 
+GO
